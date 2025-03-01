0e85b57cbac0e303297ce87caa8364384865ab94
ThanhTan1202 <DHTTgaming@gmail.com>
2025-03-01 15:34:08 +0700

    add profile page

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -5,10 +5,40 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Tài Khoản',
-        style: TextStyle(fontSize: 24),
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        const Center(
+          child: Text(
+            'Cài Đặt Tài Khoản',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(height: 10), // Add spacing
+        Expanded(
+          child: ListView(
+            children: [
+              _buildListTile('Theo dõi đơn hàng', () {}),
+              _buildListTile('Cập nhật thông tin', () {}),
+              _buildListTile('Đăng xuất', () {}),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildListTile(String title, VoidCallback onTap) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.grey[200], // Background color
+        borderRadius: BorderRadius.circular(10), // Rounded corners
+      ),
+      child: ListTile(
+        title: Text(title, style: const TextStyle(fontSize: 16)),
+        trailing: const Icon(Icons.arrow_forward_ios_rounded, size: 16),
+        onTap: onTap,
       ),
     );
   }
